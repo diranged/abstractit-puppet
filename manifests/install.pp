31cd54181efe35b0e395c2bd7ed70f72370a7675
Pete Brown <pete@abstractit.com.au>
2014-07-18 10:22:02 +1000

    changing packages installed for yum based nodes

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,17 +4,11 @@ class puppet::install (
   $puppet_version = 'installed',
   $hiera_version  = 'installed',
   $facter_version = 'installed') inherits puppet::params {
-  package { 'puppetlabs-release': ensure => latest, }
-
-  package { 'puppet-common':
-    ensure  => $puppet_version,
-    require => Package['puppetlabs-release']
-  }
+  #package { 'puppetlabs-release': ensure => latest, }
 
   package { 'puppet':
     ensure  => $puppet_version,
     require => [
-      Package['puppet-common'],
       Package['hiera'],
       Package['facter'],
       ]
--- manifests/repo.pp
+++ manifests/repo.pp
@@ -2,11 +2,13 @@
 
 class puppet::repo (
   $devel_repo = $puppet::params::devel_repo,) inherits puppet::params {
-  if ($::osfamily == 'Debian') {
-    class { 'puppet::repo::apt': devel_repo => $devel_repo, }
-
-  } elsif ($::osfamily == 'RedHat') {
-    class { 'puppet::repo::yum': devel_repo => $devel_repo, }
-
+  case $::osfamily {
+    'Debian' : {
+      class { 'puppet::repo::apt': devel_repo => $devel_repo, }
+    }
+    'RedHat' : {
+      class { 'puppet::repo::yum': devel_repo => $devel_repo, }
+    }
   }
+
 }
